99f3b31b116a4f278fa1afd6e98aa1d08e460930
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-05-12 15:27:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,41 +1,38 @@
 class Solution {
   public:
     vector<vector<int>> nearest(vector<vector<int>>& grid) {
-        // code here
         int rows = grid.size(), cols = grid[0].size();
-        vector<vector<int>> visited(rows, vector<int>(cols, -1));
         queue<pair<int, int>> q;
         for(int i=0; i<rows; i++) {
             for(int j=0; j<cols; j++) {
                 if(grid[i][j] == 1) {
-                    visited[i][j] = 0;
                     q.push({i, j});
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = -1; // Not visited
                 }
             }
         }
         
-        vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
         int dist = 0;
+        int dirs[][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+        
         while(!q.empty()) {
             int size = q.size();
+            dist++;
             while(size--) {
                 auto top = q.front();
                 q.pop();
                 int i = top.first, j = top.second;
                 for(auto& dir: dirs) {
                     int x = dir[0] + i, y = dir[1] + j;
-                    if(x >= 0 && x < rows && y >= 0 && y < cols) {
-                        if(visited[x][y] == -1) {
-                            visited[x][y] = dist + 1;
-                            q.push({x, y});
-                        }
-                    }
+                    if(x < 0 || y < 0 || x == rows || y == cols) continue;
+                    if(grid[x][y] != -1) continue;
+                    grid[x][y] = dist;
+                    q.push({x, y});
                 }
             }
-            dist++;
         }
-        
-        return visited;
+        return grid;        
     }
 };
