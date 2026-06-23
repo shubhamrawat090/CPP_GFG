9240bff54a23c554351b8da63d2d3c9f028be4a5
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-23 22:11:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char> stk;
+        unordered_map<char, char> closeToOpen;
+        closeToOpen[')'] = '(';
+        closeToOpen['}'] = '{';
+        closeToOpen[']'] = '[';
+        for(char ch: s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } else {
+                if(stk.empty()) return false;
+
+                char open = '*';
+                char close = ch;
+                for(auto& entry: closeToOpen) {
+                    if(ch == entry.first) {
+                        open = entry.second;
+                        break;
+                    }
+                }
+
+                if(open == stk.top()) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
