338b14f48ee82b0111ec8107367cbf45920dd09a
Shubham Rawat <58658277+shubhamrawat090@users.noreply.github.com>
2026-06-29 09:52:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Course Schedule II/course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites) {
+        // code here
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+        for(auto& edge: prerequisites) {
+            int u = edge[0], v = edge[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+
+        queue<int> q;
+
+        vector<int> topo;
+        for(int node=0; node<numCourses; node++) {
+            if(indegree[node] == 0) {
+                q.push(node);
+            }
+        }
+
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                int top = q.front();
+                q.pop();
+                topo.push_back(top);
+                for(int nbr: adj[top]) {
+                    indegree[nbr]--;
+                    if(indegree[nbr] == 0) {
+                        q.push(nbr);
+                    }
+                }
+            }
+        }
+        if(topo.size() != numCourses) return {};
+        return topo;
+    }
+};
